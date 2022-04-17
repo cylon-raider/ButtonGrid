df3226ec7dd4c38b46343d32d3c9ad840cd55d98
cylon-raider <markel74@msn.com>
2022-04-17 02:29:27 -0700

    Add project files.

---
+++ ButtonGrid/Controllers/ButtonController.cs
@@ -0,0 +1,49 @@
+﻿using ButtonGrid.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ButtonGrid.Controllers
+{
+    public class ButtonController : Controller
+    {
+
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 25;
+        public IActionResult Index()
+        {
+            if (buttons.Count < GRID_SIZE)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    buttons.Add(new ButtonModel { Id = i, ButtonState = random.Next(4) });
+                }
+            }
+
+
+            return View("Index", buttons);
+        }
+        public IActionResult HandleButtonClick(string buttonNumber)
+        {
+            // convert from string to int
+            int bN = int.Parse(buttonNumber);
+
+            // add one to the button state. If greater than 4, reset to 0.
+            buttons.ElementAt(bN).ButtonState = (buttons.ElementAt(bN).ButtonState + 1) % 4;
+
+
+            return View("Index", buttons);
+        }
+
+        public IActionResult ShowOneButton(int buttonNumber)
+        {
+            // add one to the button state. If greater than 4, reset to 0.
+            buttons.ElementAt(buttonNumber).ButtonState = (buttons.ElementAt(buttonNumber).ButtonState + 1) % 4;
+
+            // re-display the button that was clicked
+            return PartialView(buttons.ElementAt(buttonNumber));
+        }
+    }
+}
---
+++ ButtonGrid/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    console.log("Page is ready");
+
+    // this works for all .game-button elements that were initially loaded
+    // but will not be bound to any dynamically created buttons.    
+    //$(".game-button").click(function (event) {
+
+    // this works for any .game-button elements found on the document,
+    // even if they were dynamically created. 
+    // the click listener is attached to the document (ie. the body of the page)
+    $(document).on("click", ".game-button", function (event) {
+        event.preventDefault();
+
+        var buttonNumber = $(this).val();
+        console.log("Button number " + buttonNumber + " was clicked");
+        doButtonUpdate(buttonNumber);
+    })
+
+
+    function doButtonUpdate(buttonNumber) {
+        $.ajax({
+            datatype: "json",
+            method: 'POST',
+            url: '/button/ShowOneButton',
+            data: {
+                "buttonNumber": buttonNumber
+            },
+            success: function (data) {
+                console.log(data);
+                $("#" + buttonNumber).html(data);
+            }
+        })
+    }
+})
